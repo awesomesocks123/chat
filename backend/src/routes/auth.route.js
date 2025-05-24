e81277b1bfffb32f01da1c0d1e427e3c6a71ff8f
awesomesocks123 <143892475+awesomesocks123@users.noreply.github.com>
2025-05-24 09:34:40 -0700

    updated gitignore

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -14,5 +14,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
