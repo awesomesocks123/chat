b8a1b5697c5fda6133471db32e590c75a1680475
awesomesocks123 <143892475+awesomesocks123@users.noreply.github.com>
2025-06-05 17:51:23 -0700

    fixed navigate bug, cookies typo, and redid authuser.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -10,8 +10,8 @@ export const useAuthStore = create((set) => ({
   isCheckingAuth: true,
   checkAuth: async () => {
     try {
-      const response = await axiosInstance("/auth/check");
-      set({ authUser: response.data });
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
     } catch (error) {
       console.log("error in checkAuth", error);
       set({ authUser: null });
