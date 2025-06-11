9431f105e59d25f7e3ddbf9252d81f5a52b53353
awesomesocks123 <143892475+awesomesocks123@users.noreply.github.com>
2025-06-10 18:44:50 -0700

    removed console logs

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -22,20 +22,16 @@ export const useAuthStore = create((set) => ({
     }
   },
   signup: async (data) => {
-    console.log("Sending signup data to backend:", data); // 🧪
     set({ isSigningUp: true });
 
     try {
       const res = await axiosInstance.post("/auth/signup", data);
-      console.log("Signup response:", res.data); // 🧪
-
       set({ authUser: res.data });
       toast.success("Account created successfully");
     } catch (error) {
       const errMsg =
         error.response?.data?.message || error.message || "Signup failed";
       toast.error(errMsg);
-      console.error("Signup error:", error); // 🧪
     } finally {
       set({ isSigningUp: false });
     }
