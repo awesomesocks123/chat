0c6eff5a30ea5775dc0f95b260f210854913a9b1
awesomesocks123 <sopheakchim123@gmail.com>
2025-06-24 15:34:26 -0700

    sidebar component !

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(5001, () => {
   console.log("server is running on port " + PORT);
