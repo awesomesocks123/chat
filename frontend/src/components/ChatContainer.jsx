69682b0821e92cdfad04c7cc387a84b8f5b552f7
awesomesocks123 <sopheakchim123@gmail.com>
2025-06-25 16:55:38 -0700

    ui for send message

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,23 +1,29 @@
-import {useEffect} from "react";
-import {useChatStore} from "../store/useChatStore";
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
-
+import MessagesSkeleton from "./MessagesSkeleton";
 const ChatContainer = () => {
-  const { messages, getMessages, isMessagesLoading, selectedUser} = useChatStore()
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
 
   useEffect(() => {
-    getMessages(selectedUser._id)
-  },[selectedUser._id,getMessages])
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
 
-
-  if (isMessagesLoading ) return <div>Loading...</div>;
+  if (isMessagesLoading)
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessagesSkeleton />
+        <MessageInput />
+      </div>
+    );
   return (
     <div className="flex-1 flex flex-col overflow-auto">
-      <ChatHeader/>
+      <ChatHeader />
       <p>messages...</p>
-      <MessageInput/>
-
+      <MessageInput />
     </div>
   );
 };
